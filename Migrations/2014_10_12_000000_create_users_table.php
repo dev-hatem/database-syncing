6fdac9b25dc331c3111e74bc0e75805618395648
dev-hatem <120013301+dev-hatem@users.noreply.github.com>
2023-03-22 23:10:27 +0200

    Create 2014_10_12_000000_create_users_table.php

---
+++ Migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('is_verified')->default(false);
+            $table->string('password')->nullable();
+            $table->string('avatar')->nullable();
+            $table->boolean('is_admin')->default(true);
+            $table->boolean('status')->default(1);
+            $table->boolean('allow_2fa')->default(0)->nullable();
+            $table->text('google2fa_secret')->nullable();
+            $table->string('temp_password')->nullable();
+            $table->string('temp_email')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
