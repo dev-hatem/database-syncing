fdc02f37261ddbb5b89cddfe719c6779de04d6dc
dev-hatem <120013301+dev-hatem@users.noreply.github.com>
2023-03-22 23:08:22 +0200

    Create Tenant.php

---
+++ Models/Tenant.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Synced\Vehicle;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Models\TenantPivot;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasFactory, HasDatabase, HasDomains;
+
+}
